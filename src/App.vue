a19c5fc9422a1005279e5df52362e3d19c3bf13d
atimilson <atimilson95@gmail.com>
2019-10-21 15:56:53 -0400

    primeiro  exemplo vue

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <AddUser/>
+  </div>
+</template>
+
+<script>
+import AddUser from './components/AddUser.vue'
+
+export default { 
+  name: 'app',
+  components: {
+    AddUser
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 30px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+// boostrap
+import 'bootstrap/dist/css/bootstrap.css'
+
+// axios
+import VueAxios from 'vue-axios'
+import axios from 'axios'
+Vue.use(VueAxios, axios)
+
+// vue toasted
+import Toasted from 'vue-toasted' 
+Vue.use(Toasted, {
+  duration: 9000,
+  position: 'top-right',
+  type : 'success',
+  action : {
+    text : "OK",
+    onClick : (e, toastObject) => {
+        toastObject.goAway(0);
+    }
+  }
+})
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
